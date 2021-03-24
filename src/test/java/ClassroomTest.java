0584b184d9e3b9c21c9f03465914612247433f56
Thi Bich Loan Chung <bichloan.1032170@gmail.com>
2021-03-24 12:02:46 +0100

    create test case

--- src/test/java/ClassroomTest.java
+++ src/test/java/ClassroomTest.java
@@ -1,12 +1,25 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+import otherclasses.Classroom;
+import otherclasses.Student;
 
 public class ClassroomTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		ArrayList<Student> std = new ArrayList<Student>();
+		Classroom cl = new Classroom("Software Tester", "Spring 2021", std);
+		Student st1 = new Student("Thi", "Chung", 20, 'F');
+		Student st2 = new Student("Suu", "Tran", 18, 'M');
+		cl.addANewStudent(st1);
+		cl.addANewStudent(st2);
+		assertTrue(cl.getClassroomName().equals("Software Tester"));
+		
+		
 	}
 
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -2,11 +2,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import otherclasses.Student;
+
 public class StudentTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Student st1 = new Student("Thi", "Chung", 20, 'F', 10.0, 8.0, 9.0);
+		assertTrue(st1.getFirstName().equals("Thi"));
+		st1.calculateAverage();
+		assertEquals(st1.calculateAverage(), st1.getAverageGrade(),0);
 	}
 
 }
